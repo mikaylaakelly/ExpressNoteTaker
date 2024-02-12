107ed4a66d7148151041a35f9386119d86806076
Mikayla <149916992+mikaylaakelly@users.noreply.github.com>
2024-02-12 16:40:53 -0500

    notes not showing up on page need to fix

--- server.js
+++ server.js
@@ -8,30 +8,31 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true }));
 app.use(express.static(path.join(__dirname, '/develop/public')));
 
-const notesFilePath = '/develop/public/notes.html';
+const notesFilePath = path.join(__dirname, 'develop/public/notes.html')
 
 const readFromFile = util.promisify(fs.readFile);
+const appendFile = util.promisify(fs.appendFile);
 
 
 
 const readAndAppend = (content, file) => {
-  fs.readFile(file, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-      const parsedData = JSON.parse(data);
-      parsedData.push(content);
-      writeToFile(file, parsedData);
-    }
-  });
+  const stringContent = JSON.stringify(content);
+  return appendFile(file, stringContent, 'utf8')
+    .then(() => console.log('Note Saved YAY'))
+    .catch((err) => console.error('ERROR', err));
 };
+
+
 app.get('/api/notes', (req, res) => {
-  readFromFile(notesFilePath)
-    .then((data) => res.json(JSON.parse(data)))
-    .catch((err) => console.error(err));
+  readFromFile(notesFilePath, 'utf8')
+    .then((data) => res.send(data))
+    .catch((err) => {
+      console.error('Error reading notes:', err);
+      res.status(500).send('Internal Server Error');
+    });
 });
 
 app.post('/api/notes', (req, res) => {
@@ -51,6 +52,7 @@ app.get('/notes', (req, res) => {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'develop/public/404.html'));
 });
+
 
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`);
