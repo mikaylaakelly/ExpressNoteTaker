2782235c4432c991270397b6af5ddd16f1a3855d
Mikayla <149916992+mikaylaakelly@users.noreply.github.com>
2024-02-12 15:55:14 -0500

    deleting all notes to make it cleaner

--- server.js
+++ server.js
@@ -1,43 +1,3 @@
-// const express = require('express');
-// const path = require('path');
-// const { clog } = require('./middleware/clog');
-// const api = require('./routes/index.js');
-
-// const PORT = process.env.PORT || 3001;
-
-// const app = express();
-
-// // Import custom middleware, "cLog"
-// app.use(clog);
-
-// // Middleware for parsing JSON and urlencoded form data
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use('/api', api);
-
-// app.use(express.static('public'));
-
-// // GET Route for homepage
-// app.get('/', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/public/index.html'))
-// );
-
-// // GET Route for feedback page
-// app.get('/feedback', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
-// );
-
-// // Wildcard route to direct users to a 404 page
-// app.get('*', (req, res) =>
-//   res.sendFile(path.join(__dirname, 'public/pages/404.html'))
-// );
-
-// app.listen(PORT, () =>
-//   console.log(`App listening at http://localhost:${PORT}`)
-// );
-// MINI PROJECT EXAPMLE
-
-
 const fs = require('fs');
 const express = require('express');
 const path = require('path');
@@ -56,38 +16,6 @@ const notesFilePath = '/develop/public/notes.html';
 const readFromFile = util.promisify(fs.readFile);
 
 
-// // Promise version of fs.readFile
-// const readFromFile = util.promisify(fs.readFile);
-// /**
-//  *  Function to write data to the JSON file given a destination and some content
-//  *  @param {string} destination The file you want to write to.
-//  *  @param {object} content The content you want to write to the file.
-//  *  @returns {void} Nothing
-//  */
-// const writeToFile = (destination, content) =>
-//   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
-//     err ? console.error(err) : console.info(`\nData written to ${destination}`)
-//   );
-// /**
-//  *  Function to read data from a given a file and append some content
-//  *  @param {object} content The content you want to append to the file.
-//  *  @param {string} file The path to the file you want to save to.
-//  *  @returns {void} Nothing
-//  */
-// const readAndAppend = (content, file) => {
-//   fs.readFile(file, 'utf8', (err, data) => {
-//     if (err) {
-//       console.error(err);
-//     } else {
-//       const parsedData = JSON.parse(data);
-//       parsedData.push(content);
-//       writeToFile(file, parsedData);
-//     }
-//   });
-// };
-
-// module.exports = { readFromFile, writeToFile, readAndAppend };
-// MINI PROJECT UTILS NOTES////
 
 const readAndAppend = (content, file) => {
   fs.readFile(file, 'utf8', (err, data) => {
