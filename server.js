86cf8c3292b906e6d71899b10f14056719f7448a
Mikayla <149916992+mikaylaakelly@users.noreply.github.com>
2024-02-12 15:54:24 -0500

    fixed bug, had api defined for no reason (was using mini project as example and added in unneccessary api const

--- server.js
+++ server.js
@@ -41,20 +41,20 @@
 const fs = require('fs');
 const express = require('express');
 const path = require('path');
+const util = require('util');
+
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use('/api', api);
-
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/develop/public')));
 
+const notesFilePath = '/develop/public/notes.html';
 
-const util = require('util');
 const readFromFile = util.promisify(fs.readFile);
+
 
 // // Promise version of fs.readFile
 // const readFromFile = util.promisify(fs.readFile);
@@ -89,19 +89,28 @@ const readFromFile = util.promisify(fs.readFile);
 // module.exports = { readFromFile, writeToFile, readAndAppend };
 // MINI PROJECT UTILS NOTES////
 
+const readAndAppend = (content, file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
 app.get('/api/notes', (req, res) => {
   readFromFile(notesFilePath)
-    .then((data) => res.json(JSON.parse(data))) 
+    .then((data) => res.json(JSON.parse(data)))
     .catch((err) => console.error(err));
 });
 
 app.post('/api/notes', (req, res) => {
-  const newNote = req.body; 
+  const newNote = req.body;
   readAndAppend(newNote, notesFilePath);
   res.json(newNote);
 });
-
-
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'develop/public/index.html'));
@@ -111,11 +120,9 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'develop/public/notes.html'));
 });
 
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'develop/public/404.html'));
 });
-
 
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`);
