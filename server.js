309c5aaaaabf6ebaaf1a5b6a706ab9a5805333c1
Mikayla <149916992+mikaylaakelly@users.noreply.github.com>
2024-02-13 21:35:55 -0500

    fixing save button and add delete button

--- server.js
+++ server.js
@@ -2,26 +2,48 @@ const fs = require('fs');
 const express = require('express');
 const path = require('path');
 const util = require('util');
-
+let db = require("./Develop/db/db.json")
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, '/develop/public')));
 
-const notesFilePath = path.join(__dirname, 'develop/public/notes.html')
+const notesFilePath = path.join(__dirname, 'develop/db/db.json')
 
 const readFromFile = util.promisify(fs.readFile);
-const appendFile = util.promisify(fs.appendFile);
+const writeFile = util.promisify(fs.writeFile);
 
+
+app.delete('/api/notes/:id', (req, res) => {
+  let keepNotes = [];
+  for(let i=0;i<db.length;i++){
+    if(db[i].id != req.params.id){
+      keepNotes.push(db[i])
+    }
+  }
+  db = keepNotes;
+  const stringContent = JSON.stringify(db);
+  return writeFile(notesFilePath, stringContent, 'utf8') 
+    .then(() => {
+      console.log('Note Deleted')
+      res.send(db)
+    })
+    .catch((err) => console.error('ERROR', err));
+});
 
 
 const readAndAppend = (content, file) => {
-  const stringContent = JSON.stringify(content);
-  return appendFile(file, stringContent, 'utf8')
-    .then(() => console.log('Note Saved YAY'))
+  content.id = Math.floor(Math.random() * 1990)
+  db.push(content)
+  const stringContent = JSON.stringify(db);
+  console.log(stringContent)
+  return writeFile(file, stringContent, 'utf8') 
+    .then(() =>{ 
+
+    console.log('Note Saved YAY')})
     .catch((err) => console.error('ERROR', err));
 };
 
@@ -39,6 +61,7 @@ app.post('/api/notes', (req, res) => {
   const newNote = req.body;
   readAndAppend(newNote, notesFilePath);
   res.json(newNote);
+  
 });
 
 app.get('/', (req, res) => {
